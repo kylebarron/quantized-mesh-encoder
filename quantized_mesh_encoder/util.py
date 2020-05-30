bf7afceabaeeb9393fb26bfc539d4db4f6abc910
Kyle Barron <kylebarron2@gmail.com>
2020-05-30 16:51:04 -0600

    Fix ecef tests

--- quantized_mesh_encoder/util.py
+++ quantized_mesh_encoder/util.py
@@ -8,5 +8,8 @@ def zig_zag_encode(arr):
     https://gist.github.com/mfuerstenau/ba870a29e16536fdbaba
     (i >> bitlength-1) ^ (i << 1)
     So I right shift 15 because these arrays are int16
+
+    Note: since I'm only right-shifting 15 places, this will fail for values >
+    int16
     """
     return np.bitwise_xor(np.right_shift(arr, 15), np.left_shift(arr, 1))
