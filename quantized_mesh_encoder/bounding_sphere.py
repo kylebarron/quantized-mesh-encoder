136b68f61dfdd3929f07c4b19eed1a3deab0a227
Kyle Barron <kylebarron2@gmail.com>
2020-07-01 00:29:43 -0600

    Clean up bounding sphere code

--- quantized_mesh_encoder/bounding_sphere.py
+++ quantized_mesh_encoder/bounding_sphere.py
@@ -9,10 +9,9 @@ C code for Ritter's algorithm:
 http://www.realtimerendering.com/resources/GraphicsGems/gems/BoundSphere.c
 """
 import numpy as np
-from quantized_mesh_encoder.util_cy import ritter_second_pass
 
-def ritter_second_pass():
-    pass
+from .util_cy import ritter_second_pass
+
 
 def bounding_sphere(positions, method='naive'):
     if method == 'naive':
@@ -23,17 +22,7 @@ def bounding_sphere(positions, method='naive'):
 
     raise ValueError('Invalid bounding sphere method')
 
-# positions.shape
-#
-# positions = cartesian_positions
-# %timeit bounding_sphere_ritter(positions)
-# %timeit bounding_sphere_ritter(positions)
 
-# %time test = bounding_sphere_ritter(positions)
-# %time test = bounding_sphere_ritter(positions)
-# %timeit
-# test1 = bounding_sphere_ritter(positions)
-# test2 = bounding_sphere_ritter(positions)
 def bounding_sphere_ritter(positions):
     """
     Implements Ritter's algorithm
@@ -41,6 +30,7 @@ def bounding_sphere_ritter(positions):
     1. Find points containing minimum and maximum of each dimension.
     2. Pick pair with maximum distance
 
+    Slowest, but overall still quite fast: 304µs
     """
     # Find points containing smallest and largest component
     min_x_idx = np.where(positions[:, 0] == positions[:, 0].min())[0][0]
@@ -60,10 +50,14 @@ def bounding_sphere_ritter(positions):
 
     # Pick the pair with the maximum point-to-point separation
     # (which could be greater than the maximum dimensional span)
-    # First find which dimension this is in
+
+    # Compute x-, y-, and z-spans (distances between each component's min. and
+    # max.).
     x_span = np.linalg.norm(bbox[0] - bbox[3])
     y_span = np.linalg.norm(bbox[1] - bbox[4])
     z_span = np.linalg.norm(bbox[2] - bbox[5])
+
+    # Find largest span
     l = [x_span, y_span, z_span]
     max_idx = l.index(max(l))
 
@@ -71,44 +65,41 @@ def bounding_sphere_ritter(positions):
     min_pt = bbox[max_idx]
     max_pt = bbox[max_idx + 3]
 
+    # Calculate the center and radius of the initial sphere found by Ritter's
+    # algorithm
     center = (min_pt + max_pt) / 2
     radius = np.linalg.norm(max_pt - center)
 
     return ritter_second_pass(positions, center, radius)
 
-    # Next, each point P of S is tested for inclusion in the current ball (by
-    # simply checking that its distance from the center is less than or equal to
-    # the radius).
-    for position in positions:
-        dP = position - center
 
-        dist_to_center = np.linalg.norm(dP)
-        if dist_to_center <= radius:
-            continue
-
-        # Enlarge ball
-        # This is done by drawing a line from Pk+1 to the current center Ck of
-        # Bk and extending it further to intersect the far side of Bk.
-
-        # enlarge radius just enough
-        radius = (radius + dist_to_center) / 2
-
-        # shift center toward position
-        center += ((dist_to_center - radius) / dist_to_center) * dP
-
-    return center, radius
-
-
-def bounding_sphere_naive(positions):
-    """Naive bounding sphere method
+def bounding_sphere_from_bounding_box(positions):
+    """Create bounding sphere from axis aligned bounding box
 
     1. Find axis-aligned bounding box,
     2. Find center of box
     3. Radius is distance back to corner
+
+    Fastest method; around 70 µs
     """
     bbox = axis_aligned_bounding_box(positions)
     center = np.average(bbox, axis=0)
     radius = np.linalg.norm(center - bbox[0, :])
+    return center, radius
+
+
+def bounding_sphere_naive(positions):
+    """Create bounding sphere by checking all points
+
+    1. Find axis-aligned bounding box,
+    2. Find center of box
+    3. Find distance from center of box to every position: radius is max
+
+    Still very fast method, with tighter radius; around 160 µs
+    """
+    bbox = axis_aligned_bounding_box(positions)
+    center = np.average(bbox, axis=0)
+    radius = np.linalg.norm(center - positions, axis=1).max()
     return center, radius
 
 
