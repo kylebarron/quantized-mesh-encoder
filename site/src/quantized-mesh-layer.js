37c1e69fb020b00c4ef4a597ee5f2751d4fad805
Kyle Barron <kylebarron2@gmail.com>
2020-10-10 23:37:49 -0600

    Set tileSize for more detailed rendering

--- site/src/quantized-mesh-layer.js
+++ site/src/quantized-mesh-layer.js
@@ -56,6 +56,7 @@ export function QuantizedMeshTerrainLayer(opts) {
     onViewportLoad,
     zRange,
     refinementStrategy: "no-overlap",
+    tileSize: 256,
     updateTriggers: {
       getTileData: [meshAlgorithm, loadTexture],
     },
