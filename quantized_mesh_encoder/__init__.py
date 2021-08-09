85ce559d717b70009a9f9c92ed50da87c5ffcc32
Kyle Barron <kyle@unfolded.ai>
2021-08-08 22:15:12 -0600

    Update docs (#51)

--- quantized_mesh_encoder/__init__.py
+++ quantized_mesh_encoder/__init__.py
@@ -4,4 +4,7 @@ __author__ = """Kyle Barron"""
 __email__ = 'kylebarron2@gmail.com'
 __version__ = '0.3.1'
 
+from .constants import WGS84
+from .ellipsoid import Ellipsoid
 from .encode import encode
+from .extensions import MetadataExtension, VertexNormalsExtension, WaterMaskExtension
